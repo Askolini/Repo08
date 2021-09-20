2f8d0962d0e87e5bc3ef88a779b7ce2e1d43d1a9
Askolini <asmircc@gmail.com>
2021-09-21 01:34:59 +0200

    Fx Local Scope

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function localScope() {
+    var myVariable = "I am here!";
+    console.log(myVariable);
+}
+localScope();
+
+if (typeof myVariable != "undefined") {
+    console.log('outside localScope', myVariable)
+} else {
+    console.log('outside localScope UNDEFINED!!!')
+}
+
+module.exports = localScope;
